ec9c6cc97d9b0c6c00c591f8e7c70128ca5696e7
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-20 16:14:14 +0200

    CLI options parsing functional

---
+++ rubocop_rules_to_profile.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+class Options < Struct.new(:in_file, :out_file)
+end
+
+module CLI
+  class Parser
+    def self.parse(options) 
+      args = Options.new(nil, nil)
+      opt_parser = OptionParser.new do |opts|
+        opts.banner = "Usage: rubocop_rules_to_profile.rb [options]"
+
+        opts.on("-iIN_FILE_PATH", String, "A path to a source file with rubocop rules") do |in_file|
+          args.in_file = in_file
+        end
+
+        opts.on("-oOUT_FILE_PATH", String, "A path to an output file with rules for SQ profile file") do |out_file|
+          args.out_file = out_file
+        end
+
+        opts.on("-h", "--help", "Prints this help") do
+          puts opts
+          exit
+        end
+      end
+
+      opt_parser.parse!(options)
+      return args     
+    end
+  end
+end
+
+cli_options = begin
+                CLI::Parser.parse ARGV
+              rescue OptionParser::InvalidArgument => e
+                puts "Invalid CLI options are passed! Original exception message: #{e.message}"
+                puts "Please, run `./rubocop_rules_to_profile.rb --help` first to see all available options."
+                exit
+              end
