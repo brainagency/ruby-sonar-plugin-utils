64ad9d05832a91a400828ddaef1c7c3a6c89c5a4
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-21 15:13:45 +0200

    Completed rubocop rules to profile script

--- core/cli/to_profile/main.rb
+++ core/cli/to_profile/main.rb
@@ -1,6 +1,7 @@
 require_relative 'parser'
 require_relative '../../rubocop/parser'
 require_relative '../../sonar_qube_profile/rules/from_rubocop_rule_converter'
+require_relative '../../sonar_qube_profile/rules/to_xml'
 
 module CLI
   module ToProfile
@@ -15,6 +16,7 @@ module CLI
         cli_options = parse_argv
         rubocop_config_file = read_and_parse_rubocop_config_file(cli_options.in_file_path)
         profile_rules = convert_to_profile_rules(rubocop_config_file)
+        write_profile_xml_file(profile_rules, cli_options.out_file_path)
       end
 
       private
@@ -31,6 +33,16 @@ module CLI
       def convert_to_profile_rules(rubocop_config_file)
         rubocop_config_file.rules.map do |rubocop_rule|
           SonarQubeProfile::Rules::FromRubocopRuleConverter.new(rubocop_rule).call
+        end
+      end
+
+      def write_profile_xml_file(rules, file_path)
+        File.open(file_path, 'w') do |file|
+          rules.each do |rule|
+            xml_rule = SonarQubeProfile::Rules::ToXml.new(rule).call
+            file.write(xml_rule)
+            file.write("\n")
+          end
         end
       end
     end
