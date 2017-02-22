e4d393d8281674cdb98bb871840e8701f58b337e
Sergey Gernyak <sergeg1990@gmail.com>
2017-02-22 18:02:12 +0200

    Added rubocop rule to SQ rule definition converter

--- core/sonar_qube_rule_definition/rules/from_rubocop_rule_converter.rb
+++ core/sonar_qube_rule_definition/rules/from_rubocop_rule_converter.rb
@@ -1,12 +1,45 @@
 require_relative '../rule'
-require_relative '../../sonar_qube/rule_priorities'
-require_relative '../../sonar_qube/rule_statuses'
-require_relative '../../sonar_qube/rule_types'
+require_relative '../../sonar_qube/converters/rubocop_rule_key_to_priority'
+require_relative '../../sonar_qube/converters/rubocop_rule_key_to_status'
+require_relative '../../sonar_qube/converters/rubocop_rule_key_to_type'
 
 module SonarQubeRuleDefinition
   module Rules
     class FromRubocopRuleConverter
-      
+      attr_reader :rubocop_rule
+
+      def initialize(rubocop_rule)
+        @rubocop_rule = rubocop_rule
+      end
+
+      def call
+        SonarQubeRuleDefinition::Rule.new(
+          key: fetch_rubocop_rule_key,
+          name: rubocop_rule.description,
+          description: rubocop_rule.description,
+          priority: resolve_priority,
+          type: resolve_type,
+          status: resolve_status
+        )
+      end
+
+      private
+
+      def fetch_rubocop_rule_key
+        rubocop_rule.key
+      end
+
+      def resolve_priority
+        SonarQube::Converters::RubocopRuleKeyToPriority.new(fetch_rubocop_rule_key).call
+      end
+
+      def resolve_type
+        SonarQube::Converters::RubocopRuleKeyToType.new(fetch_rubocop_rule_key).call
+      end
+
+      def resolve_status
+        SonarQube::Converters::RubocopRuleKeyToStatus.new(fetch_rubocop_rule_key).call
+      end
     end
   end
 end
---
+++ test/core/sonar_qube_rule_definition/rules/from_rubocop_rule_converter_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+
+describe SonarQubeRuleDefinition::Rules::FromRubocopRuleConverter do
+  subject { SonarQubeRuleDefinition::Rules::FromRubocopRuleConverter.new(rubocop_rule) }
+
+  let(:rule_definition) { subject.call }
+
+  describe 'profile rule' do
+    let(:rubocop_rule) { Rubocop::Rule.new('Style/Abc', 'Abc', 'Style guide info') }
+
+    it 'returns a valid object type' do
+      assert_instance_of SonarQubeRuleDefinition::Rule, rule_definition
+    end
+
+    it 'has proper key' do
+      assert_equal rule_definition.key, rubocop_rule.key
+    end
+
+    it 'has proper name' do
+      assert_equal rule_definition.name, rubocop_rule.description
+    end
+
+    it 'has proper description' do
+      assert_equal rule_definition.description, rubocop_rule.description
+    end
+
+    it 'has proper priority' do
+      assert_equal rule_definition.priority, 'MINOR'
+    end
+
+    it 'has proper type' do
+      assert_equal rule_definition.type, 'CODE_SMELL'
+    end
+
+    it 'has proper status' do
+      assert_equal rule_definition.status, 'ready'
+    end
+  end
+end
+
